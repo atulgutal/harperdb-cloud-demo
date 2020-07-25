5e941b6a64131e7f6b99af78fe9a8c851d287e3b
atul <atulgutal007@gmail.com>
2020-07-25 10:02:14 -0700

    harperdb demo implemented

---
+++ harperdb-cloud-demo/api/index.js
@@ -0,0 +1,87 @@
+const db = require("../config/dbconfig");
+
+exports.addBook = (request, response) => {
+  db.insert(
+    {
+      table: "books",
+      records: [
+        {
+          title: request.body.title,
+          author: request.body.author
+        }
+      ]
+    },
+    (err, res) => {
+      if (err) response.status(500).json(err);
+
+      response.status(res.statusCode).json(res.data);
+    }
+  );
+};
+
+exports.getByAuthor = (request, response) => {
+  db.searchByValue(
+    {
+      table: "books",
+      searchAttribute: "author",
+      searchValue: request.body.author,
+      attributes: ["*"]
+    },
+    (err, res) => {
+      if (err) response.status(500).json(err);
+
+      console.log(res);
+
+      response.status(res.statusCode).json(res.data);
+    }
+  );
+};
+
+exports.getById = (request, response) => {
+  db.searchByHash(
+    {
+      table: "books",
+      hashValues: [request.body.id],
+      attributes: ["title"]
+    },
+    (err, res) => {
+      if (err) response.status(500).json(err);
+
+      response.status(res.statusCode).json(res.data);
+    }
+  );
+};
+
+exports.updateBook = (request, response) => {
+  db.update(
+    {
+      table: "books",
+      records: [
+        {
+          id: request.body.id,
+          title: request.body.title,
+          author: request.body.author
+        }
+      ]
+    },
+    (err, res) => {
+      if (err) response.status(500).json(err);
+
+      response.status(res.statusCode).json(res);
+    }
+  );
+};
+
+exports.deleteBook = (request, response) => {
+  db.delete(
+    {
+      table: "books",
+      hashValues: [request.body.id]
+    },
+    (err, res) => {
+      if (err) response.status(500).json(err);
+
+      response.status(res.statusCode).json(res);
+    }
+  );
+};
---
+++ harperdb-cloud-demo/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const routesController = require("./api/index");
+
+const app = express();
+const PORT = 8001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//app.get("/", (req, res) => res.json("Express Server"));
+app.route("/books").post(routesController.addBook);
+app.route("/author").post(routesController.getByAuthor);
+app.route("/search").post(routesController.getById);
+app.route("/update").post(routesController.updateBook);
+app.route("/delete").post(routesController.deleteBook);
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
